a57bd93b61d6f113a0a8f40c76686c81c910b71a
HaithemBenSlama <haithem.benslama@hotmail.fr>
2023-06-30 12:05:42 +0100

    Create PopUp deleteProduct

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,14 +1,29 @@
 "use client";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import Layout from "@/components/Layout";
+import Popup from "@/components/PopupDelete";
 import TablePagination from "@/components/TablePagination";
 import Title from "@/components/Title";
-import React from "react";
+import React, { useState } from "react";
 import { useMemo } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { PiNotePencilBold } from "react-icons/pi";
 
 export default function Products() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleDelete = () => {
+    // Perform the delete action for the product
+    console.log("Deleting product:");
+  };
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
   const columns = useMemo(
     () => [
       {
@@ -39,7 +54,10 @@ export default function Products() {
         Header: "Actions",
         Cell: ({ row }) => (
           <div className="flex gap-5">
-            <MdDeleteOutline className="text-lg hover:cursor-pointer hover:scale-150 hover:duration-300 hover:text-red-500" />
+            <MdDeleteOutline
+              className="text-lg hover:cursor-pointer hover:scale-150 hover:duration-300 hover:text-red-500"
+              onClick={openPopup}
+            />
             <PiNotePencilBold className="text-lg hover:cursor-pointer hover:scale-150 hover:duration-300 hover:text-yellow-500" />
           </div>
         ),
@@ -461,6 +479,11 @@ export default function Products() {
           },
         ]}
         categories={[{ id: 1, name: "Apple", nb_product: "15" }]}
+      />
+      <Popup
+        isOpen={isPopupOpen}
+        onClose={closePopup}
+        onDelete={handleDelete}
       />
     </Layout>
   );
