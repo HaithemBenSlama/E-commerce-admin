546ebe147cf8d71a51dd6d43de1798432ddf7bec
HaithemBenSlama <haithem.benslama@hotmail.fr>
2023-07-02 03:59:10 +0100

    Create badge component

---
+++ components/Badge.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Badge({ text, colorText, colorBg }) {
+  const style =
+    "text-xs font-medium mr-2 px-2.5 py-0.5 rounded" +
+    { colorBg } +
+    { colorText };
+  return <span className={style}>{text}</span>;
+}
