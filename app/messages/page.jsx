d838aaac5447c202c4b4b19d37eeca8a37faaf47
HaithemBenSlama <haithem.benslama@hotmail.fr>
2023-07-07 23:50:34 +0100

    Create layout for message page

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,5 +1,17 @@
+import { Breadcrumbs } from "@/components/Breadcrumbs";
 import Layout from "@/components/Layout";
+import Title from "@/components/Title";
 
 export default function Messages() {
-  return <Layout>Messages page here</Layout>;
+  return (
+    <Layout>
+      <Breadcrumbs terminalPath={"Messages"} />
+
+      <Title text={"Service Support"} />
+      <div className="px-10 py-5 flex flex-row h-full gap-1">
+        <div className="basis-1/3 bg-black rounded-lg p-3 shadow-4xl"></div>
+        <div className="basis-2/3 bg-red-800 rounded-lg shadow-2xl"></div>
+      </div>
+    </Layout>
+  );
 }
